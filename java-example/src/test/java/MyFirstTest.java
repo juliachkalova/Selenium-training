7c22060b3215de066574cc687e09adbdf6529a70
Julia Chkalova <juliachkalova@users.noreply.github.com>
2019-08-07 21:02:26 +0300

    'MyFirstTest' added

---
+++ java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    //String projectLocation = System.getProperty("user.dir");
+    //String driverLocation = System.setProperty("webdriver.chrome.driver", "C:\\Users\\Михаил\\IdeaProjects\\Selenium\\lib\\chromedriver\\chromedriver.exe");
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void MyFirstTest() {
+        driver.get("http://www.mail.ru/");
+       // wait.until(titleIs("webdriver - Search in Google"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+
+    }
+}
