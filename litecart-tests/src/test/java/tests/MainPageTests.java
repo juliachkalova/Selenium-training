6e679dd8277c2ba53caab7acffdecb95d61a6e25
Julia Chkalova <juliachkalova@users.noreply.github.com>
2019-08-15 22:46:43 +0300

    Improved locator

--- litecart-tests/src/test/java/tests/MainPageTests.java
+++ litecart-tests/src/test/java/tests/MainPageTests.java
@@ -10,39 +10,12 @@ import java.util.List;
 public class MainPageTests extends BaseTestClass {
 
     /**
-     * Test01 Find stickers on Most Popular section and verify that each product has only one sticker
+     * Test01 Find stickers and verify that each product has only one sticker
      **/
     @Test
-    public void Test01_FindStickersOnMostPopularSection() {
+    public void Test01_OnlyOneStickerPerProduct() {
         driver.get("http://localhost/litecart/");
-        WebElement mostPopularSection = driver.findElement(By.id("box-most-popular"));
-        List<WebElement> products = mostPopularSection.findElements(By.tagName("li"));
-        for (WebElement product : products) {
-            Assert.assertTrue(product.findElements(By.cssSelector(".sticker")).size() == 1);
-        }
-    }
-
-    /**
-     * Test02 Find stickers on Campaings section and verify that each product has only one sticker
-     **/
-    @Test
-    public void Test02_FindStickersOnCampaingsSection() {
-        driver.get("http://localhost/litecart/");
-        WebElement campaingsSection = driver.findElement(By.id("box-campaigns"));
-        List<WebElement> products = campaingsSection.findElements(By.tagName("li"));
-        for (WebElement product : products) {
-            Assert.assertTrue(product.findElements(By.cssSelector(".sticker")).size() == 1);
-        }
-    }
-
-    /**
-     * Test03 Find stickers on Latest Products section and verify that each product has only one sticker
-     **/
-    @Test
-    public void Test03_FindStickersOnLatestProductsSection() {
-        driver.get("http://localhost/litecart/");
-        WebElement latestProductsSection = driver.findElement(By.id("box-latest-products"));
-        List<WebElement> products = latestProductsSection.findElements(By.tagName("li"));
+        List<WebElement> products = driver.findElements(By.cssSelector("li.product"));
         for (WebElement product : products) {
             Assert.assertTrue(product.findElements(By.cssSelector(".sticker")).size() == 1);
         }
