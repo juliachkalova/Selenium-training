a60362d07ca5d8df1433f33eaa0be90a29a88543
Julia Chkalova <juliachkalova@users.noreply.github.com>
2019-08-27 10:41:48 +0300

    Update NewUserRegistrationTests.java

--- litecart-tests/src/test/java/tests/NewUserRegistrationTests.java
+++ litecart-tests/src/test/java/tests/NewUserRegistrationTests.java
@@ -10,12 +10,18 @@ import java.util.List;
 
 public class NewUserRegistrationTests extends BaseTestClass {
 
+    private static String randomEmail() {
+        return "random-" + UUID.randomUUID().toString() + "@example.com";
+    }
+
     @Test
     public void Test01_NewUserRegistration() {
         driver.get("http://localhost/litecart/");
         WebElement newUserButton = driver.findElement(By.cssSelector("div#box-account-login a"));
         newUserButton.click();
         WebElement createAccountSection = driver.findElement(By.cssSelector("div#create-account"));
+        // Generate a random email
+        final String randomEmail = randomEmail();
         driver.findElement(By.name("firstname")).sendKeys("Julia");
         driver.findElement(By.name("lastname")).sendKeys("Test120");
         driver.findElement(By.name("address1")).sendKeys("Nizhny 60302");
@@ -26,14 +32,14 @@ public class NewUserRegistrationTests extends BaseTestClass {
         countryDropdown.selectByVisibleText("United States");
         Select zoneCode = new Select (driver.findElement(By.cssSelector("select[name=zone_code]")));
         zoneCode.selectByVisibleText("New York");
-        driver.findElement(By.name("email")).sendKeys("juliatest120@mailinator.com");
+        driver.findElement(By.name("email")).sendKeys(randomEmail);
         driver.findElement(By.name("phone")).sendKeys("1001010");
         driver.findElement(By.name("password")).sendKeys("QwerAsdf");
         driver.findElement(By.name("confirmed_password")).sendKeys("QwerAsdf");
         driver.findElement(By.name("create_account")).click();
         driver.findElement(By.linkText("Logout")).click();
         WebElement loginForm = driver.findElement(By.cssSelector("form[name=login_form]"));
-        loginForm.findElement(By.cssSelector("input[name=email]")).sendKeys("juliatest120@mailinator.com");
+        loginForm.findElement(By.cssSelector("input[name=email]")).sendKeys(randomEmail);
         loginForm.findElement(By.cssSelector("input[name=password]")).sendKeys("QwerAsdf");
         loginForm.findElement(By.cssSelector("button[name=login]")).click();
         driver.findElement(By.linkText("Logout")).click();
