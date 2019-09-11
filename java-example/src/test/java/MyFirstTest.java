1a6c8b67dc6ac41daf2c58f428d021e7a464fd8e
Julia Chkalova <juliachkalova@users.noreply.github.com>
2019-09-11 06:40:05 +0300

    Add page-object package

--- java-example/src/test/java/MyFirstTest.java
+++ java-example/src/test/java/MyFirstTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -13,11 +17,12 @@ public class MyFirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
+
     //String projectLocation = System.getProperty("user.dir");
     //String driverLocation = System.setProperty("webdriver.chrome.driver", "C:\\Users\\Михаил\\IdeaProjects\\Selenium\\lib\\chromedriver\\chromedriver.exe");
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
         wait = new WebDriverWait(driver, 10);
     }
 
